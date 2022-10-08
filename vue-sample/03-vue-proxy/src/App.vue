70683e7db7d648438706d5b255a5f1453ac24441
fjzheng <1059959730@qq.com>
2022-10-08 18:26:37 +0800

    add:初步添加通过服务代理、axios调用接口获取demo

---
+++ vue-sample/03-vue-proxy/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SmsInfo></SmsInfo>
+  </div>
+</template>
+
+<script>
+import SmsInfo from './components/SmsInfo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SmsInfo
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-sample/03-vue-proxy/src/components/SmsInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click="getSmsPlan">点击获取短信计划1</button>
+        <button @click="getSmsRecord">点击获取短信记录</button>
+    </div>
+  
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    "name":"SmsTest",
+    methods:{
+        getSmsPlan(){
+            axios.get('http://localhost:8080/sms/send/plan').then(
+					response => {
+						console.log('请求短信计划成功了',response.data)
+					},
+					error => {
+						console.log('请求短信计划失败了',error.message)
+					}
+				)
+        },
+        getSmsRecord(){
+            axios.get('http://localhost:8080/sms/send/record').then(
+					response => {
+						console.log('请求短信记录成功了',response.data)
+					},
+					error => {
+						console.log('请求短信记录失败了',error.message)
+					}
+				)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
