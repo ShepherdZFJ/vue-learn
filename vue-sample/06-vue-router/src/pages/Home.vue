278d28281a75967d13352a021724d7e45a229306
fjzheng <1059959730@qq.com>
2022-10-12 13:51:20 +0800

    add：添加一级路由

---
+++ vue-sample/06-vue-router/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>我是Home主页的内容</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Home'
+
+}
+</script>
+
+<style>
+
+</style>
--- vue-sample/06-vue-router/src/router/index.js
+++ vue-sample/06-vue-router/src/router/index.js
@@ -1,6 +1,18 @@
 import VueRouter from "vue-router";
+import About from "../pages/About";
+import Home from "../pages/Home";
 
 
 export default new VueRouter({
-    
+    routes:[
+        {
+            path:"/about",
+            component:About
+        },
+        {
+            path:"/home",
+            component:Home
+        }
+    ]
+
 })
