2778f7647794bc9e36acfdd6200a06cf4016fe8a
fjzheng <1059959730@qq.com>
2022-10-13 16:02:19 +0800

    add: 引入element-ui初始化布局

--- vue-sample/vue-admin/src/router/index.js
+++ vue-sample/vue-admin/src/router/index.js
@@ -1,6 +1,12 @@
 import VueRouter from "vue-router";
-
+import Main from "@/views/Main";
 
 export default new VueRouter({
+    routes:[
+        {
+            path:'/',
+            component:Main
+        }
+    ]
     
 })
