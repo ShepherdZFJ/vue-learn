d3ba0e0179508ea0620c0d984917bba360a70747
fjzheng <1059959730@qq.com>
2022-10-19 09:49:02 +0800

    add：引入mockjs模拟后端服务

--- vue-sample/vue-admin/src/main.js
+++ vue-sample/vue-admin/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import './api/mock'
+import './api/mock'
 
 Vue.config.productionTip = false
 
