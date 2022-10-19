9bf46d108e65330227b7ab7e125605527fbb36d7
fjzheng <1059959730@qq.com>
2022-10-19 13:50:49 +0800

    add: 用户管理模块添加列表数据展示

--- vue-sample/vue-admin/src/views/User.vue
+++ vue-sample/vue-admin/src/views/User.vue
@@ -1,20 +1,86 @@
 <template>
-  <h1>我是user</h1>
+  <div class="common-tabel">
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        label="性别">
+        <template slot-scope="scope">
+            <span>{{ scope.row.sex == 1 ? '男' : '女' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="age"
+        label="年龄">
+      </el-table-column>
+      <el-table-column
+        prop="birth"
+        label="出生日期">
+      </el-table-column>
+      <el-table-column
+        prop="addr"
+        label="地址"
+        >
+      </el-table-column>
+
+      <el-table-column
+        prop="userNo"
+        label="用户编号">
+      </el-table-column>
+
+      <el-table-column
+        prop="account"
+        label="账户">
+      </el-table-column>
+
+      <el-table-column
+        prop="role"
+        label="角色">
+      </el-table-column>
+
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+      
+  </div>
 </template>
 
-<script>
+<script> 
 import { getUser } from '../api'
 export default {
     name:"User",
     data(){
       return {
+        tableData:[],
+        total:0
 
       }
     },
+    methods:{
+      // 获取列表的数据
+      getList() {
+            // 获取的列表的数据
+            getUser({params: {...this.userForm, ...this.pageData}}).then(({ data }) => {
+                console.log(data)
+                this.tableData = data.list
+
+                this.total = data.count || 0
+            })
+        }
+    },
     mounted(){
-      getUser().then(({ data }) => {
-            console.log("userList:",data)
-        })
+      this.getList()
     }
 
 }
