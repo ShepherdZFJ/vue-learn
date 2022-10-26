6a4d315f05598c62511f522efd9e54e6dc5bd24e
fjzheng <1059959730@qq.com>
2022-10-27 00:31:14 +0800

    add: :tada: 添加fast-api项目测试样例

--- vue-sample/vue-admin/src/router/index.js
+++ vue-sample/vue-admin/src/router/index.js
@@ -12,14 +12,14 @@ export default new VueRouter({
             path:'/',
             component:Main,
             redirect:'/home',
-            children:[
-                // 子路由
-                { path: 'home', name: 'home', component: Home }, // 首页
-                { path: 'user', name: 'user', component: User }, // 用户管理
-                { path: 'mall', name: 'mall', component: Mall }, // 商品管理
-                { path: 'page1', name: 'page1', component: PageOne }, // 页面1
-                { path: 'page2', name: 'page2', component: PageTwo }, // 页面2
-            ]
+            // children:[
+            //     // 子路由
+            //     { path: 'home', name: 'home', component: Home }, // 首页
+            //     { path: 'user', name: 'user', component: User }, // 用户管理
+            //     { path: 'mall', name: 'mall', component: Mall }, // 商品管理
+            //     { path: 'page1', name: 'page1', component: PageOne }, // 页面1
+            //     { path: 'page2', name: 'page2', component: PageTwo }, // 页面2
+            // ]
         }
     ]
     
