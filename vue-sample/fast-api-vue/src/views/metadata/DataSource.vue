4c83690bb585a88f7a129f0870217faab55086fe
fjzheng <1059959730@qq.com>
2022-10-27 15:15:28 +0800

    add: :rocket: 将之前的fast-api-vue的组件迁移到测试项目里

---
+++ vue-sample/fast-api-vue/src/views/metadata/DataSource.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>我是数据源</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"DataSource",
+    data(){
+      return {
+
+      }
+    },
+    mounted(){
+      console.log("dataSouce:", this)
+    }
+
+}
+</script>
+
+<style>
+
+</style>
