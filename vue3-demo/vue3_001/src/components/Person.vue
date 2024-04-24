4f7f36cfdd0e69fb36b303115aab0b3182b1c3ba
fjzheng <1059959730@qq.com>
2024-04-24 16:42:35 +0800

    update：:rocket:  setup语法糖代替vue2选项是写法

--- vue3-demo/vue3_001/src/components/Person.vue
+++ vue3-demo/vue3_001/src/components/Person.vue
@@ -8,33 +8,39 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'Person',
-  data() {
-    return {
-      name: '张三',
-      age: 18,
-      phone: '13888888888'
-    }
-  },
-  methods:{
-      // 修改姓名
-      changeName(){
-        this.name = 'zhang-san'
-      },
-      // 修改年龄
-      changeAge(){
-        this.age += 1
-      },
-      // 展示联系方式
-      showTel(){
-        alert(this.phone)
-      }
-    }
-
 }
 </script>
+
+<!-- 下面的写法是setup语法糖 -->
+<script setup lang="ts">
+ import {ref} from 'vue'
+  // name和age是一个RefImpl的实例对象，简称ref对象，它们的value属性是响应式的。
+  let name = ref('张三')
+  let age = ref(18)
+  // tel就是一个普通的字符串，不是响应式的
+  let tel = '13888888888'
+
+  function changeName(){
+    // JS中操作ref对象时候需要.value
+    name.value = '李四'
+    console.log(name.value)
+
+    // 注意：name不是响应式的，name.value是响应式的，所以如下代码并不会引起页面的更新。
+    // name = ref('zhang-san')
+  }
+  function changeAge(){
+    // JS中操作ref对象时候需要.value
+    age.value += 1 
+    console.log(age.value)
+  }
+  function showTel(){
+    alert(tel)
+  }
+</script>
+
 
 <style scoped>
   .person {
