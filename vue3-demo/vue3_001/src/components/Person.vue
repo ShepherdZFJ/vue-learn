3178be1723cd245e1d164ea72baa073c770a887e
fjzheng <1059959730@qq.com>
2024-04-24 16:53:38 +0800

    update：reactive创建响应式对象

--- vue3-demo/vue3_001/src/components/Person.vue
+++ vue3-demo/vue3_001/src/components/Person.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>    
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="showTel">查看联系方式</button>
+    <h2>汽车信息：一台{{ car.brand }}汽车，价值{{ car.price }}万</h2>
+    <h2>游戏列表：</h2>
+    <ul>
+      <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+    </ul>
+    <h2>测试：{{obj.a.b.c.d}}</h2>
+    <button @click="changeCarPrice">修改汽车价格</button>
+    <button @click="changeFirstGame">修改第一游戏</button>
+    <button @click="test">测试</button>
   </div>
 </template>
 
@@ -14,31 +18,35 @@ export default {
 }
 </script>
 
-<!-- 下面的写法是setup语法糖 -->
-<script setup lang="ts">
-    import {ref} from 'vue'
-  // name和age是一个RefImpl的实例对象，简称ref对象，它们的value属性是响应式的。
-  let name = ref('张三')
-  let age = ref(18)
-  // tel就是一个普通的字符串，不是响应式的
-  let tel = '13888888888'
+<script lang="ts" setup >
+import { reactive } from 'vue'
 
-  function changeName(){
-    // JS中操作ref对象时候需要.value
-    name.value = '李四'
-    console.log(name.value)
+// 数据
+let car = reactive({ brand: '奔驰', price: 100 })
+let games = reactive([
+  { id: 'ahsgdyfa01', name: '英雄联盟' },
+  { id: 'ahsgdyfa02', name: '王者荣耀' },
+  { id: 'ahsgdyfa03', name: '原神' }
+])
+let obj = reactive({
+  a:{
+    b:{
+      c:{
+        d:666
+      }
+    }
+  }
+})
 
-    // 注意：name不是响应式的，name.value是响应式的，所以如下代码并不会引起页面的更新。
-    // name = ref('zhang-san')
-  }
-  function changeAge(){
-    // JS中操作ref对象时候需要.value
-    age.value += 1 
-    console.log(age.value)
-  }
-  function showTel(){
-    alert(tel)
-  }
+function changeCarPrice() {
+  car.price += 10
+}
+function changeFirstGame() {
+  games[0].name = '流星蝴蝶剑'
+}
+function test(){
+  obj.a.b.c.d = 999
+}
 </script>
 
 
